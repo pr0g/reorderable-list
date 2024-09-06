d2f856d6e1653c823a6f62719bdc029b7d6356fc
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-09-06 20:18:18 +0100

    add eslint support

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,26 @@
-import { experimental_useEffectEvent, useState } from "react";
+import { experimental_useEffectEvent, useEffect, useState } from "react";
 
 export const App = () => {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("John");
+  const [combined, setCombined] = useState("");
   
-  experimental_useEffectEvent(() => {
-  });
+  useEffect(() => {
+    setCombined(name + ' ' + count);
+  }, [count]);
   
+  useEffect(() => {
+    console.log(combined);
+  },[combined]);
+
+  experimental_useEffectEvent(() => {});
+
   return (
     <>
       <div></div>
-      <h1 className="text-3xl font-bold underline bg-gray-300">DEMO APP</h1>
-      <button onClick={() => setCount(count + 1)}>The count is: {count}</button>
+      <h1 className="sm:text-3xl text-2xl font-bold underline bg-gray-300">DEMO APP</h1>
+      <button className="block" onClick={() => setCount(count + 1)}>The count is: {count}</button>
+      <button className="block" onClick={() => setName((name) => name + 'a')}>Change name</button>
     </>
   );
 };
