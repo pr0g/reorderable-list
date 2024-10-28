3a2ca6944d4984d56b7715fee46df931c3048328
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2024-10-28 18:39:38 +0000

    add just changed state to improve animation on release

--- src/reorderable-list.tsx
+++ src/reorderable-list.tsx
@@ -1,4 +1,4 @@
-import { useState, Fragment, useRef } from "react";
+import { useState, Fragment, useRef, useEffect } from "react";
 
 export function ReorderableList() {
   const [items] = useState<string[]>([
@@ -14,6 +14,13 @@ export function ReorderableList() {
 
   const [movingIndex, setMovingIndex] = useState(-1);
   const [availableIndex, setAvailableIndex] = useState(-1);
+  const [justChangedIndex, setJustChangedIndex] = useState(-1);
+
+  useEffect(() => {
+    if (justChangedIndex !== -1) {
+      setJustChangedIndex(-1);
+    }
+  }, [justChangedIndex]);
 
   const mouseDownVerticalPosition = useRef(0);
   const [mouseDelta, setMouseDelta] = useState(0);
@@ -26,12 +33,15 @@ export function ReorderableList() {
   const getStyle = (
     index: number,
     movingIndex: number,
+    justChangedIndex: number,
     availableIndex: number,
     mouseDelta: number,
     itemHeight: number
   ) => {
     const style: React.CSSProperties = {};
-    if (index === movingIndex) {
+    if (index === justChangedIndex) {
+      style.transform = `scale(1.1)`;
+    } else if (index === movingIndex) {
       style.transform = `translateY(${mouseDelta}px) scale(1.1)`;
     } else if (index >= availableIndex && index < movingIndex) {
       style.transform = `translateY(${itemHeight}px)`;
@@ -61,6 +71,7 @@ export function ReorderableList() {
                   const [extracted] = items.splice(movingIndex, 1);
                   items.splice(availableIndex, 0, extracted);
                   e.currentTarget.releasePointerCapture(e.pointerId);
+                  setJustChangedIndex(availableIndex);
                   setMovingIndex(-1);
                   setAvailableIndex(-1);
                   setMouseDelta(0);
@@ -109,13 +120,14 @@ export function ReorderableList() {
                   }
                 }}
                 className={`select-none bg-slate-500 rounded-lg my-1 ${
-                  index === movingIndex
+                  index === movingIndex || index === justChangedIndex
                     ? "text-red-500"
                     : "text-black transition-transform duration-300"
                 }`}
                 style={getStyle(
                   index,
                   movingIndex,
+                  justChangedIndex,
                   availableIndex,
                   mouseDelta,
                   itemHeight
