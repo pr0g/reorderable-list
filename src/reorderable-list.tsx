d9ab0b65c5c3a088762a58bf6347d6deaa05420d
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-01-22 20:36:42 +0000

    horizontal - no animation

--- src/reorderable-list.tsx
+++ src/reorderable-list.tsx
@@ -17,9 +17,10 @@ export function ReorderableList() {
   ]);
   const [movingIndex, setMovingIndex] = useState(-1);
   const [availableIndex, setAvailableIndex] = useState(-1);
-  const [justChangedIndex, setJustChangedIndex] = useState(-1);
-  const [justChangedMouseDelta, setJustChangedMouseDelta] = useState([0, 0]);
-  const [justChangedHoverIndex, setJustChangedHoverIndex] = useState(-1);
+  const [justReleasedMouseDelta, setJustReleasedMouseDelta] = useState([0, 0]);
+  // const [becomingUnavailableIndex, setBecomingUnavailableIndex] = useState(-1);
+  const [justReleasedIndex, setJustReleasedIndex] = useState(-1);
+  // const [becomingAvailableIndex, setBecomingAvailableIndex] = useState(-1);
   const [mouseDelta, setMouseDelta] = useState([0, 0]);
   const mouseDownPosition = useRef([0, 0]);
   const ulRef = useRef<HTMLUListElement | null>(null);
@@ -28,11 +29,11 @@ export function ReorderableList() {
   const itemWidth = useRef(0);
 
   useEffect(() => {
-    if (justChangedIndex !== -1) {
-      setJustChangedIndex(-1);
-      setJustChangedMouseDelta([0, 0]);
+    if (justReleasedIndex !== -1) {
+      setJustReleasedIndex(-1);
+      setJustReleasedMouseDelta([0, 0]);
     }
-  }, [justChangedIndex]);
+  }, [justReleasedIndex]);
 
   useEffect(() => {
     return () => {
@@ -42,42 +43,61 @@ export function ReorderableList() {
 
   const getStyle = useCallback(
     (
+      // item: string,
       index: number,
       movingIndex: number,
-      hoveredIndex: number,
-      justChangedIndex: number,
+      // becomingAvailableIndex: number,
+      justReleasedIndex: number,
       availableIndex: number,
-      mouseDelta: [number, number],
+      // mouseDelta: [number, number],
       itemWidth: number
     ) => {
-      const theIndex =
-        movingIndex !== -1
-          ? movingIndex
-          : justChangedHoverIndex !== -1
-          ? justChangedHoverIndex
-          : -1;
+      const theIndex = movingIndex;
+      // movingIndex !== -1
+      //   ? movingIndex
+      //   : becomingAvailableIndex !== -1
+      //   ? becomingAvailableIndex
+      //   : -1;
       const style: React.CSSProperties = {};
-      if (index === hoveredIndex || index === movingIndex) {
+      if (
+        /* index === becomingAvailableIndex || */
+        index === movingIndex
+      ) {
         style.transform = `scale(1.1)`;
         style.position = "relative";
         style.left = `${mouseDelta[0]}px`;
         style.top = `${mouseDelta[1]}px`;
-      } else if (index === justChangedIndex) {
-        style.transform = `translateX(${justChangedMouseDelta[0]}px) translateY(${justChangedMouseDelta[1]}px) scale(1.1)`;
-      } else if (index >= availableIndex && index < theIndex) {
-        style.transform = `translateX(${itemWidth}px)`;
-      } else if (index <= availableIndex && index > theIndex) {
-        style.transform = `translateX(${-itemWidth}px)`;
+      } else if (index === justReleasedIndex) {
+        // console.log("just released");
+        // console.log(JSON.stringify(justReleasedMouseDelta));
+        style.transform = `translateX(${justReleasedMouseDelta[0]}px) translateY(${justReleasedMouseDelta[1]}px) scale(1.1)`;
+      } else if (
+        index >= availableIndex &&
+        index < theIndex &&
+        theIndex !== -1
+      ) {
+        // style.transform = `translateX(${itemWidth}px)`;
+        style.position = "relative";
+        style.left = itemWidth;
+      } else if (
+        index <= availableIndex &&
+        index > theIndex &&
+        theIndex !== -1
+      ) {
+        // style.transform = `translateX(${-itemWidth}px)`;
+        style.position = "relative";
+        style.left = -itemWidth;
       }
       return style;
     },
-    [justChangedHoverIndex, justChangedMouseDelta]
+    [justReleasedMouseDelta, mouseDelta]
   );
 
   const onPointerDown = useCallback(
-    (index: number, e: React.PointerEvent<HTMLLIElement>) => {
+    (index: number) => (e: React.PointerEvent<HTMLLIElement>) => {
       setAvailableIndex(index);
-      setJustChangedHoverIndex(index);
+      // setBecomingAvailableIndex(index);
+      setMovingIndex(index);
       liRef.current = e.currentTarget;
       // note: space-x-2 is 8px (see +8 below)
       itemWidth.current = liRef.current.clientWidth + 8;
@@ -88,44 +108,64 @@ export function ReorderableList() {
   );
 
   const onPointerUp = useCallback(
-    (e: React.PointerEvent<HTMLLIElement>) => {
+    (index: number) => (e: React.PointerEvent<HTMLLIElement>) => {
       // cancel move before animation ends and sets movingIndex
+      liRef.current = null;
       if (movingIndex === -1) {
+        console.log("pointer up");
+        // setBecomingUnavailableIndex(becomingAvailableIndex);
+        // setJustReleasedIndex(becomingAvailableIndex);
         setAvailableIndex(-1);
-        setJustChangedHoverIndex(-1);
+        // setBecomingAvailableIndex(-1);
         e.currentTarget.releasePointerCapture(e.pointerId);
+        setJustReleasedMouseDelta([mouseDelta[0], mouseDelta[1]]);
         setMouseDelta([0, 0]);
         return;
       }
       e.currentTarget.releasePointerCapture(e.pointerId);
+      // setBecomingUnavailableIndex(availableIndex);
+      setJustReleasedIndex(availableIndex);
+      ///
       const newItems = [...items];
       const [extracted] = newItems.splice(movingIndex, 1);
       newItems.splice(availableIndex, 0, extracted);
       setItems(newItems);
-      setJustChangedIndex(availableIndex);
-      setJustChangedMouseDelta([
+      ///
+      setJustReleasedMouseDelta([
         mouseDelta[0] + (movingIndex - availableIndex) * itemWidth.current,
         mouseDelta[1],
       ]);
       setMovingIndex(-1);
       setAvailableIndex(-1);
       setMouseDelta([0, 0]);
-      liRef.current = null;
+
+      // if (index === becomingAvailableIndex) {
+      // setMovingIndex(index);
+      // setBecomingAvailableIndex(-1);
+      // }
     },
-    [availableIndex, items, mouseDelta, movingIndex]
+    [
+      availableIndex,
+      /* becomingAvailableIndex, */ items,
+      mouseDelta,
+      movingIndex,
+    ]
   );
 
   const onPointerMove = useCallback(
     (e: React.PointerEvent<HTMLLIElement>) => {
-      if (justChangedHoverIndex !== -1 || availableIndex !== -1) {
-        setMouseDelta([
-          e.clientX - mouseDownPosition.current[0],
-          e.clientY - mouseDownPosition.current[1],
-        ]);
+      if (/* becomingAvailableIndex !== -1 || */ availableIndex !== -1) {
+        requestAnimationFrame(() => {
+          setMouseDelta([
+            e.clientX - mouseDownPosition.current[0],
+            e.clientY - mouseDownPosition.current[1],
+          ]);
+        });
       }
       if (availableIndex === -1) {
         return;
       }
+      console.log("move");
       const indexBefore = availableIndex - 1;
       if (indexBefore >= 0) {
         if (liRef.current) {
@@ -142,7 +182,6 @@ export function ReorderableList() {
           }
         }
       }
-
       const indexAfter = availableIndex + 1;
       if (indexAfter < items.length) {
         if (liRef.current) {
@@ -160,7 +199,7 @@ export function ReorderableList() {
         }
       }
     },
-    [availableIndex, items.length, justChangedHoverIndex]
+    [availableIndex, items.length /* , becomingAvailableIndex */]
   );
 
   return (
@@ -170,42 +209,45 @@ export function ReorderableList() {
       <ul ref={ulRef} className="flex flex-wrap gap-x-2">
         {items.map((item, index) => {
           return (
-            <Fragment key={`${index}-${item}`}>
-              <li
-                key={`${index}-${item}`}
-                onTransitionEnd={() => {
-                  if (index === justChangedHoverIndex) {
-                    setMovingIndex(index);
-                    setJustChangedHoverIndex(-1);
-                  }
-                }}
-                onPointerDown={(e) => {
-                  onPointerDown(index, e);
-                }}
-                onPointerUp={onPointerUp}
-                onPointerMove={onPointerMove}
-                className={`select-none bg-slate-500 rounded-lg my-1 min-w-24 text-center ${
-                  index === movingIndex || index === justChangedIndex
-                    ? "text-red-500 z-50"
-                    : "text-black transition-transform duration-300"
-                }`}
-                style={getStyle(
-                  index,
-                  movingIndex,
-                  justChangedHoverIndex,
-                  justChangedIndex,
-                  availableIndex,
-                  mouseDelta as [number, number],
-                  itemWidth.current
-                )}
-              >
-                {item}
-              </li>
-            </Fragment>
+            // <Fragment key={`${item}`}>
+            <li
+              key={item}
+              // onTransitionEnd={() => {
+              //   // if (index === becomingAvailableIndex) {
+              //   //   setMovingIndex(index);
+              //   //   setBecomingAvailableIndex(-1);
+              //   // }
+              //   // if (index === becomingUnavailableIndex) {
+              //   //   setBecomingUnavailableIndex(-1);
+              //   // }
+              // }}
+              onPointerDown={onPointerDown(index)}
+              onPointerUp={onPointerUp(index)}
+              onPointerMove={onPointerMove}
+              className={`select-none bg-slate-500 rounded-lg my-1 min-w-24 text-center ${
+                index === movingIndex ? "text-red-500 z-50" : "text-black" // transition-transform duration-300
+              }`}
+              style={getStyle(
+                // item,
+                index,
+                movingIndex,
+                // becomingAvailableIndex,
+                justReleasedIndex,
+                availableIndex,
+                // mouseDelta as [number, number],
+                itemWidth.current
+              )}
+            >
+              {item}
+            </li>
+            // </Fragment>
           );
         })}
       </ul>
       <p>Moving index: {movingIndex}</p>
+      <p>Available index: {availableIndex}</p>
+      {/* <p>Becoming available index: {becomingAvailableIndex}</p> */}
+      {/* <p>Becoming unavailable index: {becomingUnavailableIndex}</p> */}
     </div>
   );
 }
