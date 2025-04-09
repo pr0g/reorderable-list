77839a756639404ff263a8bd1cc6c0273f0d0921
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-04-09 10:10:33 +0100

    switch to useReducer

--- src/reorderable-list.tsx
+++ src/reorderable-list.tsx
@@ -1,4 +1,12 @@
-import { useState, useRef, useCallback, useEffect, useMemo, memo } from "react";
+import {
+  useState,
+  useRef,
+  useCallback,
+  useEffect,
+  useMemo,
+  memo,
+  useReducer,
+} from "react";
 
 // helper function to get unscaled rect
 const getUnscaledRect = (element: HTMLElement, scale: number) => {
@@ -25,6 +33,79 @@ const getUnscaledRect = (element: HTMLElement, scale: number) => {
   };
 };
 
+type Action =
+  | { type: "JUST_RELEASED" }
+  | { type: "TRANSITION_ENDED"; index: number }
+  | { type: "POINTER_DOWN"; index: number }
+  | { type: "POINTER_UP_EARLY"; delta: [number, number] }
+  | { type: "POINTER_UP"; delta: [number, number] }
+  | { type: "POINTER_MOVE"; delta: [number, number] }
+  | { type: "CHANGE_INDEX"; indexChange: number };
+
+type State = {
+  movingIndex: number;
+  availableIndex: number;
+  justReleasedIndex: number;
+  justReleasedMouseDelta: [number, number];
+  justPressedIndex: number;
+  mouseDelta: [number, number];
+};
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "JUST_RELEASED":
+      return {
+        ...state,
+        justReleasedIndex: -1,
+      };
+    case "TRANSITION_ENDED":
+      return {
+        ...state,
+        movingIndex: action.index,
+        justPressedIndex: -1,
+      };
+    case "POINTER_DOWN":
+      return {
+        ...state,
+        availableIndex: action.index,
+        justPressedIndex: action.index,
+        justReleasedMouseDelta: [0, 0],
+        mouseDelta: [0, 0],
+      };
+    case "POINTER_UP_EARLY":
+      return {
+        ...state,
+        justReleasedIndex: state.justPressedIndex,
+        justReleasedMouseDelta: action.delta,
+        availableIndex: -1,
+        justPressedIndex: -1,
+        mouseDelta: [0, 0],
+      };
+    case "POINTER_UP":
+      return {
+        ...state,
+        justReleasedMouseDelta: action.delta,
+        justReleasedIndex: state.availableIndex,
+        movingIndex: -1,
+        availableIndex: -1,
+        mouseDelta: [0, 0],
+      };
+    case "POINTER_MOVE":
+      return {
+        ...state,
+        mouseDelta: action.delta,
+      };
+    case "CHANGE_INDEX": {
+      return {
+        ...state,
+        availableIndex: state.availableIndex + action.indexChange,
+      };
+    }
+    default:
+      return state;
+  }
+}
+
 export const ReorderableList = memo(function ReorderableList() {
   const [items, setItems] = useState<string[]>([
     "Item 1",
@@ -47,12 +128,16 @@ export const ReorderableList = memo(function ReorderableList() {
 
   const columns = 3;
 
-  const [movingIndex, setMovingIndex] = useState(-1);
-  const [availableIndex, setAvailableIndex] = useState(-1);
-  const [justReleasedIndex, setJustReleasedIndex] = useState(-1);
-  const [justReleasedMouseDelta, setJustReleasedMouseDelta] = useState([0, 0]);
-  const [justPressedIndex, setJustPressedIndex] = useState(-1);
-  const [mouseDelta, setMouseDelta] = useState([0, 0]);
+  const initialState: State = {
+    movingIndex: -1,
+    availableIndex: -1,
+    justReleasedIndex: -1,
+    justPressedIndex: -1,
+    justReleasedMouseDelta: [0, 0],
+    mouseDelta: [0, 0],
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const ulRef = useRef<HTMLUListElement | null>(null);
   const liRef = useRef<HTMLLIElement | null>(null);
@@ -61,10 +146,10 @@ export const ReorderableList = memo(function ReorderableList() {
   const itemHeight = useRef(0);
 
   useEffect(() => {
-    if (justReleasedIndex !== -1) {
-      setJustReleasedIndex(-1);
+    if (state.justReleasedIndex !== -1) {
+      dispatch({ type: "JUST_RELEASED" });
     }
-  }, [justReleasedIndex]);
+  }, [state.justReleasedIndex]);
 
   const getStyle = useCallback(
     (
@@ -80,10 +165,10 @@ export const ReorderableList = memo(function ReorderableList() {
       if (index === pressedIndex || index === movingIndex) {
         style.transform = `scale(1.1)`;
         style.position = "relative";
-        style.left = `${mouseDelta[0]}px`;
-        style.top = `${mouseDelta[1]}px`;
+        style.left = `${state.mouseDelta[0]}px`;
+        style.top = `${state.mouseDelta[1]}px`;
       } else if (index === releasedIndex) {
-        style.transform = `translateX(${justReleasedMouseDelta[0]}px) translateY(${justReleasedMouseDelta[1]}px) scale(1.1)`;
+        style.transform = `translateX(${state.justReleasedMouseDelta[0]}px) translateY(${state.justReleasedMouseDelta[1]}px) scale(1.1)`;
       } else if (
         index >= availableIndex &&
         index < movingIndex &&
@@ -120,25 +205,21 @@ export const ReorderableList = memo(function ReorderableList() {
       }
       return style;
     },
-    [mouseDelta, justReleasedMouseDelta]
+    [state.mouseDelta, state.justReleasedMouseDelta]
   );
 
   const onTransitionEnd = useCallback(
     (index: number) => () => {
-      if (index === justPressedIndex) {
-        setMovingIndex(index);
-        setJustPressedIndex(-1);
+      if (index === state.justPressedIndex) {
+        dispatch({ type: "TRANSITION_ENDED", index });
       }
     },
-    [justPressedIndex]
+    [state.justPressedIndex]
   );
 
   const onPointerDown = useCallback(
     (index: number) => (e: React.PointerEvent<HTMLLIElement>) => {
-      setAvailableIndex(index);
-      setJustPressedIndex(index);
-      setJustReleasedMouseDelta([0, 0]);
-      setMouseDelta([0, 0]);
+      dispatch({ type: "POINTER_DOWN", index });
       liRef.current = e.currentTarget;
       // note: m-1 is 8px (see +8 below)
       itemWidth.current = liRef.current.clientWidth + 8;
@@ -151,60 +232,59 @@ export const ReorderableList = memo(function ReorderableList() {
 
   const onPointerUp = useCallback(
     (e: React.PointerEvent<HTMLLIElement>) => {
-      const deltaX = mouseDelta[0];
-      const deltaY = mouseDelta[1];
+      const deltaX = state.mouseDelta[0];
+      const deltaY = state.mouseDelta[1];
 
       liRef.current = null;
       e.currentTarget.releasePointerCapture(e.pointerId);
-      if (movingIndex === -1) {
-        setJustReleasedIndex(justPressedIndex);
-        setJustReleasedMouseDelta([deltaX, deltaY]);
-        setAvailableIndex(-1);
-        setJustPressedIndex(-1);
-        setMouseDelta([0, 0]);
+      if (state.movingIndex === -1) {
+        dispatch({ type: "POINTER_UP_EARLY", delta: [deltaX, deltaY] });
         return;
       }
 
       const newItems = [...items];
-      const [extracted] = newItems.splice(movingIndex, 1);
-      newItems.splice(availableIndex, 0, extracted);
+      const [extracted] = newItems.splice(state.movingIndex, 1);
+      newItems.splice(state.availableIndex, 0, extracted);
       setItems(newItems);
 
-      const colm = movingIndex % columns;
-      const rowm = Math.floor(movingIndex / columns);
-      const cola = availableIndex % columns;
-      const rowa = Math.floor(availableIndex / columns);
-      setJustReleasedMouseDelta([
-        deltaX + (colm - cola) * itemWidth.current,
-        deltaY + (rowm - rowa) * itemHeight.current,
-      ]);
-      setJustReleasedIndex(availableIndex);
-      setMovingIndex(-1);
-      setAvailableIndex(-1);
-      setMouseDelta([0, 0]);
+      const colm = state.movingIndex % columns;
+      const rowm = Math.floor(state.movingIndex / columns);
+      const cola = state.availableIndex % columns;
+      const rowa = Math.floor(state.availableIndex / columns);
+
+      dispatch({
+        type: "POINTER_UP",
+        delta: [
+          deltaX + (colm - cola) * itemWidth.current,
+          deltaY + (rowm - rowa) * itemHeight.current,
+        ],
+      });
     },
-    [availableIndex, items, justPressedIndex, mouseDelta, movingIndex]
+    [items, state.availableIndex, state.mouseDelta, state.movingIndex]
   );
 
   const onPointerMove = useCallback(
     (e: React.PointerEvent<HTMLLIElement>) => {
-      if (justPressedIndex !== -1 || availableIndex !== -1) {
-        setMouseDelta([
-          e.clientX - mouseDownPosition.current[0],
-          e.clientY - mouseDownPosition.current[1],
-        ]);
+      if (state.justPressedIndex !== -1 || state.availableIndex !== -1) {
+        dispatch({
+          type: "POINTER_MOVE",
+          delta: [
+            e.clientX - mouseDownPosition.current[0],
+            e.clientY - mouseDownPosition.current[1],
+          ],
+        });
       }
 
-      if (availableIndex === -1) {
+      if (state.availableIndex === -1) {
         return;
       }
 
       const rows = Math.ceil(items.length / columns);
-      const availableRow = Math.floor(availableIndex / columns);
+      const availableRow = Math.floor(state.availableIndex / columns);
 
       if (liRef.current) {
         // drag left
-        const indexBefore = availableIndex - 1;
+        const indexBefore = state.availableIndex - 1;
         const indexBeforeRow = Math.floor(indexBefore / columns);
         if (indexBefore >= 0 && indexBeforeRow === availableRow) {
           const left =
@@ -215,14 +295,12 @@ export const ReorderableList = memo(function ReorderableList() {
             getUnscaledRect(liRef.current, 1.1).left <
             left + itemWidth.current / 2
           ) {
-            setAvailableIndex(
-              (currentAvailableIndex) => currentAvailableIndex - 1
-            );
+            dispatch({ type: "CHANGE_INDEX", indexChange: -1 });
           }
         }
 
         // drag right
-        const indexAfter = availableIndex + 1;
+        const indexAfter = state.availableIndex + 1;
         const indexAfterRow = Math.floor(indexAfter / columns);
         if (indexAfter < items.length && indexAfterRow === availableRow) {
           const left =
@@ -233,13 +311,11 @@ export const ReorderableList = memo(function ReorderableList() {
             getUnscaledRect(liRef.current, 1.1).right >=
             left + itemWidth.current / 2
           ) {
-            setAvailableIndex(
-              (currentAvailableIndex) => currentAvailableIndex + 1
-            );
+            dispatch({ type: "CHANGE_INDEX", indexChange: 1 });
           }
         }
 
-        const nextRow = Math.floor((availableIndex + columns) / columns);
+        const nextRow = Math.floor((state.availableIndex + columns) / columns);
         if (nextRow < rows) {
           const top =
             ulRef.current!.getBoundingClientRect().top +
@@ -248,13 +324,11 @@ export const ReorderableList = memo(function ReorderableList() {
             getUnscaledRect(liRef.current, 1.1).bottom >=
             top + itemHeight.current / 2
           ) {
-            setAvailableIndex(
-              (currentAvailableIndex) => currentAvailableIndex + columns
-            );
+            dispatch({ type: "CHANGE_INDEX", indexChange: columns });
           }
         }
 
-        const prevRow = Math.floor((availableIndex - columns) / columns);
+        const prevRow = Math.floor((state.availableIndex - columns) / columns);
         if (prevRow >= 0) {
           const top =
             ulRef.current!.getBoundingClientRect().top +
@@ -263,14 +337,12 @@ export const ReorderableList = memo(function ReorderableList() {
             getUnscaledRect(liRef.current, 1.1).top <
             top + itemHeight.current / 2
           ) {
-            setAvailableIndex(
-              (currentAvailableIndex) => currentAvailableIndex - columns
-            );
+            dispatch({ type: "CHANGE_INDEX", indexChange: -columns });
           }
         }
       }
     },
-    [availableIndex, items.length, justPressedIndex]
+    [state.availableIndex, items.length, state.justPressedIndex]
   );
 
   const gridStyle = useMemo(
@@ -293,18 +365,18 @@ export const ReorderableList = memo(function ReorderableList() {
               onPointerUp={onPointerUp}
               onPointerMove={onPointerMove}
               className={`select-none bg-slate-500 rounded-lg m-1 min-w-24 text-center ${
-                index === movingIndex
+                index === state.movingIndex
                   ? "text-red-500 z-50"
-                  : index === justReleasedIndex
+                  : index === state.justReleasedIndex
                   ? "text-black z-50"
                   : "transition-transform duration-300"
               }`}
               style={getStyle(
                 index,
-                movingIndex,
-                justPressedIndex,
-                justReleasedIndex,
-                availableIndex,
+                state.movingIndex,
+                state.justPressedIndex,
+                state.justReleasedIndex,
+                state.availableIndex,
                 itemWidth.current,
                 itemHeight.current
               )}
@@ -314,8 +386,8 @@ export const ReorderableList = memo(function ReorderableList() {
           );
         })}
       </ul>
-      <p>Moving index: {movingIndex}</p>
-      <p>Available index: {availableIndex}</p>
+      <p>Moving index: {state.movingIndex}</p>
+      <p>Available index: {state.availableIndex}</p>
     </div>
   );
 });
